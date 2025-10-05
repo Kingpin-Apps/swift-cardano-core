78becfa9873c04b6c94c35115a67d90819b40d6c
Hareem Adderley <haddderley@kingpinapps.com>
2025-10-05 16:16:12 -0500

    fix: add description and from era

--- Sources/SwiftCardanoCore/Types/Era.swift
+++ Sources/SwiftCardanoCore/Types/Era.swift
@@ -8,6 +8,39 @@ public enum Era: String, CBORSerializable, Equatable, CaseIterable, Sendable {
     case babbage
     case conway
     
+    public var description: String {
+        switch self {
+            case .byron: return "byron"
+            case .shelley: return "shelley"
+            case .allegra: return "allegra"
+            case .mary: return "mary"
+            case .alonzo: return "alonzo"
+            case .babbage: return "babbage"
+            case .conway: return "conway"
+        }
+    }
+    
+    public init(from era: String) {
+        switch era.lowercased() {
+            case "byron":
+                self = .byron
+            case "shelley":
+                self = .shelley
+            case "allegra":
+                self = .allegra
+            case "mary":
+                self = .mary
+            case "alonzo":
+                self = .alonzo
+            case "babbage":
+                self = .babbage
+            case "conway":
+                self = .conway
+            default:
+                self = .conway
+        }
+    }
+    
     public init(from primitive: Primitive) throws {
         guard case let .string(eraString) = primitive,
               let era = Era(rawValue: eraString.lowercased()) else {
