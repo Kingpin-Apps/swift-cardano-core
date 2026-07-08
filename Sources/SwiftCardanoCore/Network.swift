0e157d8692a720fac1644a245908962278b46aca
Hareem Adderley <hadderley@kingpinapps.com>
2026-07-08 08:41:04 -0500

    feat(network): add Codable and CustomStringConvertible conformances

--- Sources/SwiftCardanoCore/Network.swift
+++ Sources/SwiftCardanoCore/Network.swift
@@ -76,3 +76,42 @@ public enum Network: Sendable, Equatable {
         }
     }
 }
+
+// MARK: - Codable
+
+extension Network: CustomStringConvertible {}
+
+extension Network: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let stringValue = try? container.decode(String.self) {
+            switch stringValue.lowercased() {
+            case "mainnet": self = .mainnet
+            case "preprod": self = .preprod
+            case "preview": self = .preview
+            case "guildnet": self = .guildnet
+            case "sanchonet": self = .sanchonet
+            default:
+                if let magic = Int(stringValue) { self = .custom(magic) } else { self = .mainnet }
+            }
+        } else if let intValue = try? container.decode(Int.self) {
+            self = .custom(intValue)
+        } else {
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Invalid network value")
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .mainnet: try container.encode("mainnet")
+        case .preprod: try container.encode("preprod")
+        case .preview: try container.encode("preview")
+        case .guildnet: try container.encode("guildnet")
+        case .sanchonet: try container.encode("sanchonet")
+        case .custom(let magic): try container.encode(magic)
+        }
+    }
+}
